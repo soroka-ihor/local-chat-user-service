6c5dbc12716a4d9ad990d811ff744d3b6250486b
Ihor Soroka <ihor.soroka@inmost.pro>
2024-07-15 15:59:03 +0300

    test

--- src/main/java/es/local/chat/user/service/LocalChatUserServiceApplication.java
+++ src/main/java/es/local/chat/user/service/LocalChatUserServiceApplication.java
@@ -10,6 +10,7 @@ public class LocalChatUserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LocalChatUserServiceApplication.class, args);
+
     }
 
 }
